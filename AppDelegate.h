2ce3102f57e8c40ff6bf99730fd0298f1f13ab5b
elly304 <40547769+elly304@users.noreply.github.com>
2018-07-12 23:23:43 +0200

    Add files via upload

---
+++ AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SAMUF2
+//
+//  Created by stefono on 28/06/18.
+//  Copyright (c) 2018 stefano.aru. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
